0ddf04b3aaf1875eb54d9492a94522619c0d7972
Tianlong Nan <80225506+ntlfi@users.noreply.github.com>
2026-09-11 14:48:33 -0400

    clean up the old files

--- new_py.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-ax = plt.figure().add_subplot(projection='3d')
-ax.view_init(elev=30, azim=45, roll=30)
-
-plt.show()
--- test.py
+++
@@ -1,5 +0,0 @@
-import time
-
-for _ in range(100): 
-    print("o", end ="", flush=True)
-    time.sleep(0.1)
